24d5d93eea770baf918a192ddb7ed2f69d3dbf2e
Özgür İnce <ozgur@ince.ist>
2020-03-14 02:13:32 +0300

    modify 56

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class Ball : MonoBehaviour
 {
     private Paddle paddle;
     private Vector2 distance;
+    private bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,32 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (hasStarted) return;
+        LockBallToPaddle();
+        LaunchOnClick();
+    }
+
+    private void LaunchOnClick()
+    {
+        hasStarted = Input.GetMouseButtonDown(0);
+        if (!hasStarted) return;
+        GetComponent<Rigidbody2D>().velocity = GetVelocity();
+    }
+
+    private void LockBallToPaddle()
+    {
+        transform.position = (Vector2)paddle.transform.position + GetVector();
+    }
+
+    private Vector2 GetVector()
+    {
         var time = Time.realtimeSinceStartup;
         var angle = Mathf.Pow(-1, Mathf.FloorToInt(time) % 2) * (45 - (time - Mathf.Floor(time)) * 90);
-        transform.position = paddle.transform.position + Quaternion.Euler(0, 0, angle) * distance;
+        return Quaternion.Euler(0, 0, angle) * distance;
+    }
+
+    private Vector2 GetVelocity()
+    {
+        return GetVector() * 20;
     }
 }
