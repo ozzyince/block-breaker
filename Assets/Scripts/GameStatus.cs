3d481f88c2196eab850111f3f77bc93600906bf5
Özgür İnce <ozgur@ince.ist>
2020-03-15 22:20:58 +0300

    modify 67

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(1, 9)] [SerializeField] int gameSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = Mathf.Pow(1.25f, gameSpeed - 5);
+    }
+}
