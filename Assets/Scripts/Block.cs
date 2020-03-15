dc01353f9cef7f5a5604a6a4dc4289662bff7d3c
Özgür İnce <ozgur@ince.ist>
2020-03-15 11:56:00 +0300

    modify 65

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,14 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, transform.position);
