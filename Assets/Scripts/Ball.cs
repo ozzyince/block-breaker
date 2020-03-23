7d07f8b85ab21d338bfe647d13444e3ddb3b1662
Özgür İnce <ozgur@ince.ist>
2020-03-24 00:25:56 +0300

    modify 78

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
@@ -9,6 +7,7 @@ public class Ball : MonoBehaviour
     private Paddle paddle;
     private Vector2 distance;
     private AudioSource audioSource;
+    private Rigidbody2D rigidBody;
     private bool hasStarted = false;
 
     // Start is called before the first frame update
@@ -17,6 +16,7 @@ public class Ball : MonoBehaviour
         paddle = FindObjectOfType<Paddle>();
         distance = transform.position - paddle.transform.position;
         audioSource = GetComponent<AudioSource>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -31,7 +31,7 @@ public class Ball : MonoBehaviour
     {
         hasStarted = Input.GetMouseButtonDown(0);
         if (!hasStarted) return;
-        GetComponent<Rigidbody2D>().velocity = GetVelocity();
+        rigidBody.velocity = GetVelocity();
     }
 
     private void LockBallToPaddle()
@@ -55,5 +55,6 @@ public class Ball : MonoBehaviour
     {
         if (!hasStarted) return;
         audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
+        rigidBody.velocity = Quaternion.Euler(0, 0, Random.Range(-5f, 5f)) * rigidBody.velocity;
     }
 }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Level : MonoBehaviour
 {
@@ -20,8 +18,7 @@ public class Level : MonoBehaviour
 
     public void BlockDestroyed()
     {
-        --breakableBlocks;
-        if (breakableBlocks > 0) return;
+        if (--breakableBlocks > 0) return;
         sceneLoader.LoadNextScene();
     }
 }
